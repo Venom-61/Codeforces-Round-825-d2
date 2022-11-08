fe816431cc276df873b690afb69c168797bfcee7
Venom-61 <subhendukarmakar61@gmail.com>
2022-11-08 10:14:53 +0530

    Good Subarrays (Easy Version)

--- c.cpp
+++ c.cpp
@@ -5,18 +5,44 @@ using namespace std;
 
 // C. Good Subarrays (Easy version)
 
-void solve() {
-    int n;
-    cin >> n;
-    vector<ll> dp(n+5, 0);
-    ll ans = 0;
-    for(int i = 1; i <= n; i++) {
-        ll x; cin >> x;
-        dp[i] = min(dp[i-1] + 1, x);
-        ans += dp[i];
-    }
-    cout << ans << "\n";
+/* Greedy approach */
+
+void solve(){
+	int n;
+	cin >> n;
+	vector<int> a(n);
+	for(int i = 0; i < n; i++)
+		cin >> a[i];
+
+    long long count = 1, ans = 0;
+	int left = 0, right = 0;
+	while(left < n) {
+		while(right < n and count <= a[right]) {
+			right++;
+			count++;
+		}
+		ans += count - 1;
+		left++;
+		count--;
+	}
+	cout << ans << endl;
 }
+
+/* DP Approach */
+
+/* void solve() { */
+/*     int n; */
+/*     cin >> n; */
+/*     vector<ll> dp(n+5, 0); */
+/*     ll ans = 0; */
+/*     for(int i = 1; i <= n; i++) { */
+/*         ll x; */ 
+/*         cin >> x; */
+/*         dp[i] = min(dp[i-1] + 1, x); */
+/*         ans += dp[i]; */
+/*     } */
+/*     cout << ans << "\n"; */
+/* } */
 
 int main() {
     ios_base::sync_with_stdio(false);
